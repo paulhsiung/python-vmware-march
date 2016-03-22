fcfdd25194014c075e19bddd69437ab8d86cdb98
Paul Hsiung <phsiung@socialcast.com>
2016-03-22 15:33:31 -0700

    type lab

--- excercises/data_structures.py
+++ excercises/data_structures.py
@@ -1,47 +1,53 @@
 
 def evens(n):
     """
-    Return a list of all integers greater than 0 and up 
+    Return a list of all integers greater than 0 and up
     to (and including) `n` that are even.
 
     @param: n (positive, numeric)
 
     Test with:
-    $ py.test tests/test_syntax.py::SyntaxExcercises::test_evens
+    $ py.test tests/test_ds.py::DataStructureExcercises::test_evens
     """
-    return None
+    return [i+1 for i in range(1,n,2)]
 
 
 def counter(elements):
     """
-    Given a list of elements, return a dictionary where 
+    Given a list of elements, return a dictionary where
     each key is an element, and the value is the integer number of times
-    it appeared in the original list. 
+    it appeared in the original list.
 
     @param: elements (list)
 
     Test with:
-    $ py.test tests/test_syntax.py::DataStructureExcercises::test_counter
+    $ py.test tests/test_ds.py::DataStructureExcercises::test_counter
     """
-    return None
+    ret = {}
+    print elements
+    for key in set(elements):
+        ret[key] = elements.count(key)
+    return ret
 
 
 def csv_line(elements, sep):
     """
-    Given a list of elements make a string that is each object's 
-    string representation, separated by the `sep` string. 
+    Given a list of elements make a string that is each object's
+    string representation, separated by the `sep` string.
 
         >>> print csv_line([0, None, 'apple'], ",")
         0,None,apple
 
-    HINT: You may need to convert elements to strings. 
+    HINT: You may need to convert elements to strings.
 
     BONUS POINTS: Do this in a single line! Look up the `join()` function.
 
     Test with:
     $ py.test tests/test_ds.py::DataStructureExcercises::test_csv_line
     """
-    return None
+    print sep
+    print elements
+    return sep.join([str(e) for e in elements])
 
 
 def similar(first, second):
@@ -54,14 +60,14 @@ def similar(first, second):
     Test with:
     $ py.test tests/test_ds.py::DataStructureExcercises::test_similar
     """
-    return None
+    return list(set(first).intersection(set(second)))
 
 
 def filter_and_rank_teams(teams, min_score, restricted_teams, top_n):
     """
-    Given a list of (int score, team name) tuples, and an integer min_score, 
-    return a list consisting of only tuples where first score value is 
-    greater than or equal to min_score AND if that team name is NOT in 
+    Given a list of (int score, team name) tuples, and an integer min_score,
+    return a list consisting of only tuples where first score value is
+    greater than or equal to min_score AND if that team name is NOT in
     the restricted_teams list.
 
     The final list must be sorted by score of the tuple, descending.
@@ -75,9 +81,13 @@ def filter_and_rank_teams(teams, min_score, restricted_teams, top_n):
     HINT: List comprehensions, sorting, and slicing will be useful here!
 
     Test with:
-    $ py.test tests/test_ds.py::DataStructureExcercises::test_filter_tuples
+    $ py.test tests/test_ds.py::DataStructureExcercises::test_filter_and_rank_teams
     """
-    return None
+    teams = [(s,n) for (s,n) in teams
+                        if s >= min_score and n not in restricted_teams ]
+
+    teams.sort(key=lambda tup: tup[0], reverse=True)
+    return teams[:top_n]
 
 
 
